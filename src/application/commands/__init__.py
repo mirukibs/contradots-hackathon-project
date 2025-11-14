32d73b8a49f3000c15414655403319609d0b9f5d
Kibwana Miruru <mirukibs12.km@gmail.com>
2025-11-14 21:46:22 +0300

    fixed the init for commands and dtos for ease recognition

--- src/application/commands/__init__.py
+++ src/application/commands/__init__.py
@@ -1,5 +1,6 @@
 """Commands package - Command objects for write operations"""
 
+from .authentication_commands import AuthenticateUserCommand, LoginCommand, LogoutCommand
 from .register_person_command import RegisterPersonCommand
 from .create_activity_command import CreateActivityCommand
 from .submit_action_command import SubmitActionCommand
@@ -7,6 +8,9 @@ from .deactivate_activity_command import DeactivateActivityCommand
 from .validate_proof_command import ValidateProofCommand
 
 __all__ = [
+    'AuthenticateUserCommand',
+    'LoginCommand', 
+    'LogoutCommand',
     'RegisterPersonCommand',
     'CreateActivityCommand',
     'SubmitActionCommand',
